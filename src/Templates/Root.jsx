c9c0049b0f053bcb39238b6928ec1def8a494acc
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-10-31 20:01:35 +0200

    fix errors

--- src/Templates/Root.jsx
+++ src/Templates/Root.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import UserDashboard from "../Pages/UserDashboard";
 import SidebarContainer from "../Organisms/SidebarContainer";
-import PageHeader from "../Molecules/header/pageHeader";
+// import PageHeader from "../Molecules/header/pageHeader";
 
 export default function Root() {
   return (
     <main className="flex justify-end bg-white">
       <div className="flex flex-col w-full overflow-auto">
-        <PageHeader />
+        {/* <PageHeader /> */}
         <span className="overflow-auto bg-[#F6F6F6]">
           <UserDashboard />
         </span>
